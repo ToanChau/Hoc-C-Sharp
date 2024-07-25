eb555295ccde671c2c150923a4d2a14b6ced32ca
Chau Duc Toan <toanpl060@gmail.com>
2024-07-25 23:05:22 +0700

    thêm Lamda

--- lamda/Program.cs
+++ lamda/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Thêm không gian tên System.Linq để sử dụng ToList()
 
-
-namespace lamda
+namespace lamda // Lamda - Biểu thức vô danh
 {
     public class Program
     {
-        static void Main(string[] main)
+        static void Main(string[] args)
         {
             Action<string> thongbao;
             thongbao = (string s) => System.Console.WriteLine(s);
+            // biểu thức lamda khi được tạo phải được gán cho 1 biến dạng delegate
+            // có thể rút gon lại 
+            thongbao = (chuoi) => System.Console.WriteLine(chuoi);
+            // vì thongbao hiểu được kiểu của tham số
 
-            thongbao.Invoke("xin chào");
-            
+            //biểu thức lamda không có tham so
+            Action none;
+            none = () => {   
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("Goi tu lamda khong tham so");
+                Console.ResetColor();
+            };
+
+            thongbao?.Invoke("hello gọi từ lamda2");
+            none?.Invoke();
+
+            // kết hợp với thư viện ToList, foreach,....
+            string[] mang = {"Nhi","Yeu","Dau"};
+            mang.ToList().ForEach((string x) => { 
+                if (x.Length>0) {
+                     System.Console.WriteLine(x);
+                }
+                   
+            });
+
+
+            List<string> kq = mang.Where( (x) => x.Length>0).ToList();
+            kq.ForEach((string x) => {System.Console.WriteLine(x);}) ;
         }
     }
 }
--- lamda/obj/Debug/net8.0/lamda.AssemblyInfo.cs
+++ lamda/obj/Debug/net8.0/lamda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lamda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4b0b818e627e046e3bf2907dcee1ec78c43321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898371ca1c82839aa9f1166072017a7b1ab2a37e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lamda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lamda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
